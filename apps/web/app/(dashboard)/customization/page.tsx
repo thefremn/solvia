d99121320df0aeccb9e35899d0074f73ebe116fd
Amar Kumar Thakur <kumaramar4880@gmail.com>
2025-12-12 10:16:57 +0530

    26: widget-settings-partial

--- apps/web/app/(dashboard)/customization/page.tsx
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -1,8 +1,8 @@
+import { CustomizationView } from "@/modules/customization/ui/views/customization_view";
+
 const Page = () => {
     return (
-        <div>
-            Customization
-        </div>
+        <CustomizationView/>
     )
 }
 
---
+++ apps/web/modules/customization/ui/components/customization-form.tsx
@@ -0,0 +1,137 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { Button } from "@workspace/ui/components/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+import { Input } from "@workspace/ui/components/input";
+import { Separator } from "@workspace/ui/components/separator";
+import { Textarea } from "@workspace/ui/components/textarea";
+import { Doc } from "@workspace/backend/_generated/dataModel";
+import { api } from "@workspace/backend/_generated/api";
+import { useMutation } from "convex/react";
+export const widgetSettingsSchema = z.object({
+  greetMessage: z.string().min(1, "Greeting message is required"),
+  defaultSuggestions: z.object({
+    suggestion1: z.string().optional(),
+    suggestion2: z.string().optional(),
+    suggestion3: z.string().optional(),
+  }),
+  vapiSettings: z.object({
+    assistantId: z.string().optional(),
+    phoneNumber: z.string().optional(),
+  }),
+});
+
+type WidgetSettings = Doc<"widgetSettings">;
+type FormSchema = z.infer<typeof widgetSettingsSchema>;
+
+interface CustomizationFormProps {
+  initialData: WidgetSettings | null;
+  hasVapiPlugin: boolean;
+}
+
+export const CustomizationForm = ({ 
+    initialData, }: 
+    CustomizationFormProps) => {
+        const upertWidgetSettings = useMutation(api.private.widgetSettings.upsert);
+        const form = useForm<FormSchema>({
+            resolver: zodResolver(widgetSettingsSchema),
+            defaultValues: {
+                greetMessage: initialData?.greetMessage || "Hi! How can I help you today?",
+                defaultSuggestions: {
+                    suggestion1: initialData?.defaultSuggestions?.suggestion1 || "",
+                    suggestion2: initialData?.defaultSuggestions?.suggestion2 || "",
+                    suggestion3: initialData?.defaultSuggestions?.suggestion3 || "",
+                },
+                vapiSettings: {
+                    assistantId: initialData?.vapiSettings?.assistantId || "",
+                    phoneNumber: initialData?.vapiSettings?.phoneNumber || "",
+                },
+            },
+        })
+        const onSubmit = async (values: FormSchema) => {
+            try {
+                const vapiSettings: WidgetSettings["vapiSettings"] = {
+                    assistantId: values.vapiSettings.assistantId === "none"
+                    ? ""
+                    : values.vapiSettings.assistantId,
+                    phoneNumber: values.vapiSettings.phoneNumber === "none"
+                    ? ""
+                    : values.vapiSettings.phoneNumber,
+                };
+                await upertWidgetSettings({
+                    greetMessage: values.greetMessage,
+                    defaultSuggestions: values.defaultSuggestions,
+                    vapiSettings,
+                });
+                toast.success("Widget settings saved");
+            } catch (error) {
+                console.error(error);
+                toast.error("Something went wrong");
+            }
+        }
+        return (
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>General Chat Settings</CardTitle>
+                            <CardDescription>
+                                Configure basic chat widget behavoiur and messages
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-6">
+                            <FormField
+                                control={form.control}
+                                name="greetMessage"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Greeting Message</FormLabel>
+                                        <FormControl>
+                                            <Textarea
+                                             {...field} 
+                                             placeholder="Welcome message shown when the chat open"
+                                             rows={3}
+                                             />
+                                        </FormControl>
+                                        <FormDescription>
+                                            The first message customers see when they open the chat
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Separator />
+                            <div className="space-y-4">
+                                <div>
+                                    <h3 className="text-sm mb-4">
+                                        Default Suggestions
+                                    </h3>
+                                    <p className="mb-4 text-sm text-muted-foreground">
+                                        Quick reply suggestions shown to customers to help guide the conversation
+                                    </p>
+                                </div>
+                            </div>    
+                        </CardContent>
+                    </Card>
+                </form>
+            </Form>
+        );
+};
+
