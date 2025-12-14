1baf6473007b1322f8c96e1b7832ef65b1a35053
chintondutta <chintondutta.tech@gmail.com>
2025-12-14 16:30:05 +0530

    29: widget-improvements

--- apps/widget/modules/widget/ui/screens/widget-voice-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-voice-screen.tsx
@@ -41,7 +41,7 @@ export const WidgetVoiceScreen = () => {
                 </div>
             </WidgetHeader>
             {transcript.length > 0 ? (
-                <AIConversation className="h-full flex-1">
+                <AIConversation className="h-full">
                     <AIConversationContent>
                         {transcript.map((message, index) => (
                             <AIMessage
--- apps/widget/modules/widget/ui/views/widget.view.tsx
+++ apps/widget/modules/widget/ui/views/widget.view.tsx
@@ -9,6 +9,7 @@ import { WidgetSelectionScreen } from "../screens/widget-selection-screen";
 import { WidgetChatScreen } from "../screens/widget-chat-screen";
 import { WidgetInboxScreen } from "../screens/widget-inbox-screen";
 import { WidgetVoiceScreen } from "../screens/widget-voice-screen";
+import { WidgetContactScreen } from "../screens/widget-contact-screen";
 
 interface Props {
   organizationId: string | null;
@@ -25,12 +26,11 @@ export const WidgetView = ({ organizationId }: Props) => {
    inbox: <WidgetInboxScreen />,
    selection: <WidgetSelectionScreen/>,   
    chat: <WidgetChatScreen/>,
-   contact: <p>TODO: Contact</p>,
+   contact: <WidgetContactScreen />,
    
   }
   return (
-    //TODO: Confirm wheather or not "min-h-screen min-w-screen" is nedeed
-    <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+    <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
         {screenComponents[screen]}
     </main>
   );
